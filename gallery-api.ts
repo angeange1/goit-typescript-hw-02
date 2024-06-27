2db7842ebb862792cfc47591675a746ffd0f922c
angeange1 <149048056+angeange1@users.noreply.github.com>
2024-06-28 01:52:21 +0300

    c2

--- gallery-api.ts
+++ gallery-api.ts
@@ -1,9 +1,15 @@
 import axios from "axios";
+import { GalleryItems } from "./src/components/App/App.types";
 
 axios.defaults.baseURL = "https://api.unsplash.com/search";
 
-export const getPhotos<T> = async (topic: string, currentPage: number) => {
-  const response = await axios.get("/photos/?client_id=WVvkoWB7pFdggoXbNMLId4SllYX7T7ZTvyTx777_Fak", {
+type ApiResponse = {
+  results: GalleryItems[];
+  total: number;
+}
+
+export const getPhotos = async (topic: string, currentPage: number): Promise<ApiResponse> => {
+  const response = await axios.get<ApiResponse>("/photos/?client_id=WVvkoWB7pFdggoXbNMLId4SllYX7T7ZTvyTx777_Fak", {
     params: {
       query: topic,
       page: currentPage,
